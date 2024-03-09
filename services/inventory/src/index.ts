c63e55f02fb17dddccd185f1f351ff043859b6ed
Imran Hasan <imran@stacklearner.com>
2024-03-10 05:07:18 +0600

    Update health endpoint response message

--- services/inventory/src/index.ts
+++ services/inventory/src/index.ts
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(morgan('dev'));
 
 app.get('/health', (_req, res) => {
-	res.status(200).json({ status: 'UP' });
+	res.status(200).json({ status: 'Inventory is UP' });
 });
 
 // app.use((req, res, next) => {
