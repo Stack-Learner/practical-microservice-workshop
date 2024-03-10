7f4d68862c1fc0c3f5ab5d61ada1ca0b061d25d0
Imran Hasan <imran@stacklearner.com>
2024-03-10 20:16:00 +0600

    Update email service workflow and health endpoint

--- services/email/src/index.ts
+++ services/email/src/index.ts
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(morgan('dev'));
 
 app.get('/health', (_req, res) => {
-	res.status(200).json({ status: 'Email Service is UP' });
+	res.status(200).json({ status: 'UP' });
 });
 
 // app.use((req, res, next) => {
