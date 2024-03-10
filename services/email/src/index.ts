d89584775e88c07e89b5b3f5fddb69733ba883ee
Imran Hasan <imran@stacklearner.com>
2024-03-10 20:40:10 +0600

    Fix typo in health endpoint response

--- services/email/src/index.ts
+++ services/email/src/index.ts
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(morgan('dev'));
 
 app.get('/health', (_req, res) => {
-	res.status(200).json({ status: 'UP and Running and' });
+	res.status(200).json({ status: 'UP and and' });
 });
 
 // app.use((req, res, next) => {
