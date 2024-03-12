854fb38b6f9832351865d3102efdd53ca37f8699
Imran Hasan <imran@stacklearner.com>
2024-03-12 17:40:46 +0600

    Update email service configuration

--- services/email/src/config.ts
+++ services/email/src/config.ts
@@ -1,9 +1,14 @@
 import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
-	host: process.env.SMTP_HOST || 'host.docker.internal',
-	port: parseInt(process.env.SMTP_PORT || '1025'),
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.GOOGLE_SMTP_USER, // your gmail address
+    pass: process.env.GOOGLE_SMTP_PASSWORD, // your gmail password or app-specific password
+  },
 });
 
 export const defaultSender =
-	process.env.DEFAULT_SENDER_EMAIL || 'admin@example.com';
+  process.env.DEFAULT_SENDER_EMAIL || 'admin@example.com';
